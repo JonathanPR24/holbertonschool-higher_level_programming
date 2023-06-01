6983ec987b2b2ce20568a193d3d00f3d1c0c8fd1
JonathanPR24 <jonathanperez604@gmail.com>
2023-05-31 20:20:11 -0400

    comparing last number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,8 @@ number = random.randint(-10000, 10000)
 
 # Get the last digit of the number
 last_digit = abs(number) % 10
+if number < 0:
+    last_digit *= -1
 
 # Print the last digit and its comparison
 print("Last digit of", number, "is", last_digit, end=" ")
