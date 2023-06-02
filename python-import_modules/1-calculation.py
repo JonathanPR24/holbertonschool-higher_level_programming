292a116d60923a431ded16c24f2be39377081026
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-02 16:06:09 -0400

    imports functions and imports functions

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-add_result = add(a, b)
-sub_result = sub(a, b)
-mul_result = mul(a, b)
-div_result = div(a, b)
+    addition_result = add(a, b)
+    subtraction_result = sub(a, b)
+    multiplication_result = mul(a, b)
+    division_result = div(a, b)
 
-print("{} + {} = {}".format(a, b, add_result))
-print("{} - {} = {}".format(a, b, sub_result))
-print("{} * {} = {}".format(a, b, mul_result))
-print("{} / {} = {}".format(a, b, div_result))
+    print(f"Result of addition: {addition_result}")
+    print(f"Result of subtraction: {subtraction_result}")
+    print(f"Result of multiplication: {multiplication_result}")
+    print(f"Result of division: {division_result}")
+
