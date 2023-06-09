87787294899393cd359e98a512204e192594b374
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-09 15:16:01 -0400

    divides 2 integers and prints the result

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,10 +3,11 @@ def safe_print_division(a, b):
     try:
         result = a / b
     except ZeroDivisionError:
-        return None
+        result = None
     finally:
         try:
             print("Inside result: {}".format(result))
         except NameError:
             pass
+        print("{} / {} = {}".format(a, b, result))
     return result
