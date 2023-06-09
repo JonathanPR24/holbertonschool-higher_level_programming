4dddc6f79c15ea5ad9b96c6bcd0968f756134b51
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-09 15:18:08 -0400

    divides 2 integers and prints the result

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -7,7 +7,7 @@ def safe_print_division(a, b):
     finally:
         try:
             print("Inside result: {}".format(result))
+            print("{} / {} = {}".format(a, b, result))
         except NameError:
             pass
-        print("{} / {} = {}".format(a, b, result))
     return result
