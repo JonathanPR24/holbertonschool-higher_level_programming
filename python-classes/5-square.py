04e852f5449e7423b9049d5c8f59ced4fd270374
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-14 18:15:58 -0400

    Square that defines a square by: based on 4-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """A class representing a square."""
+
+    def __init__(self, size=0):
+        """Initializes a new Square instance.
+
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square with the character '#'."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print('#' * self.__size)
