183116797da0b8106e3f7afe128be4adc0f223ea
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-15 01:58:32 -0400

    defines a rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+    """
+    A class that represents a rectangle.
+    
+    Attributes:
+        No attributes defined.
+    """
+
+class Rectangle:
+    """
+    Initializes a Rectangle object.
+
+    Args:
+        No arguments.
+    """
+    pass
