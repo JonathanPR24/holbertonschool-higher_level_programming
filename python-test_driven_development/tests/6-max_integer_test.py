a6e889e20526c796dfb3d2d6fdc6ba5e5942e98d
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-15 17:04:16 -0400

    addign new tests

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,42 +1,27 @@
-#!/usr/bin/python3
-"""
-This module contains a unittest class for the function max_integer().
-"""
-
 import unittest
-from 6-max_integer import max_integer
+from max_integer import max_integer
 
-class TestMaxInteger(unittest.TestCase):
-    """
-    A unittest class for max_integer()
-    """
-    
-    def test_basic(self):
-        """
-        Test normal cases for the function.
-        """
+
+class MaxIntegerTestCase(unittest.TestCase):
+    def test_max_integer(self):
+        # Test case 1: Regular list
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+        # Test case 2: List with unordered elements
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
+
+        # Test case 3: List with negative numbers
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
-        self.assertEqual(max_integer([4]), 4)
+
+        # Test case 4: List with duplicate maximum values
+        self.assertEqual(max_integer([1, 2, 4, 4]), 4)
+
+        # Test case 5: Empty list
         self.assertIsNone(max_integer([]))
-    def test_string(self):
-        """
-        Test if a TypeError is raised when the list contains strings.
-        """
-        with self.assertRaises(TypeError):
-            max_integer(['a', 'b', 'c'])
-    def test_non_list(self):
-        """
-        Test if a TypeError is raised when the input is a string.
-        """
-        with self.assertRaises(TypeError):
-            max_integer("string")
-    def test_nested_list(self):
-        """
-        Test if a TypeError is raised when the list contains other lists.
-        """
-        with self.assertRaises(TypeError):
-            max_integer([[1, 2, 3], [4, 5, 6]])
+
+        # Test case 6: List with a single element
+        self.assertEqual(max_integer([5]), 5)
+
+
 if __name__ == '__main__':
     unittest.main()
