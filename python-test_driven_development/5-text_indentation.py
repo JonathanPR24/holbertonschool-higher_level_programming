7d6e8deb9e947d8ab5ba33c307110704a951ebb4
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-15 16:30:21 -0400

    prints a text with 2 new lines after each of these characters: ., ? and

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,19 +6,23 @@ This module contains a function that indents text
 
 def text_indentation(text):
     """
-    Prints a text with 2 new lines after each of these characters: '.', '?', and ':'.
+    Prints a text with 2 new lines after each of these characters: ., ? and :
 
     Args:
-        text: The text to print. Must be a string.
+        text (str): The text to be indented.
 
     Raises:
         TypeError: If text is not a string.
-    """
 
+    Note:
+        There should be no space at the beginning or
+        at the end of each printed line.
+    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    for char in ".:?":
-        text = text.replace(char, "{}\n\n".format(char))
-
-    print("\n".join(line.strip() for line in text.split("\n")))
+    punctuation_marks = [".", "?", ":"]
+    for char in text:
+        print(char, end="")
+        if char in punctuation_marks:
+            print("\n\n", end="")
