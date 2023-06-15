d1868130c812c0f5606f541032f10c22152d0734
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-15 15:28:01 -0400

    divides all elements of a matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,14 +4,13 @@ def matrix_divided(matrix, div):
     Divides all elements of a matrix by a given divisor.
 
     Args:
-        matrix (list): A matrix represented as a
-        list of lists of integers or floats.
-        div (int or float): The divisor to
-        divide the matrix elements by.
+        matrix (list): A matrix represented as a list of lists of integers
+                       or floats.
+        div (int or float): The divisor to divide the matrix elements by.
 
     Returns:
-        list: A new matrix with elements divided by
-        the divisor, rounded to 2 decimal places.
+        list: A new matrix with elements divided by the divisor, rounded to
+              2 decimal places.
 
     Raises:
         TypeError: If matrix is not a list of lists of integers/floats,
@@ -19,10 +18,10 @@ def matrix_divided(matrix, div):
                    or if div is not a number.
         ZeroDivisionError: If div is equal to 0.
     """
-    if not isinstance(matrix, list) or any
-    (not isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists)
-                of integers/floats")
+    if not isinstance(matrix, list) or any(
+            not isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
 
     if any(len(row) != len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
