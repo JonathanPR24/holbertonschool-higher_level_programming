c025002be24fbb06a4f5960a6e68d2b5ba989111
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-15 21:08:33 -0400

    prints a text with 2 new lines after each of these characters: ., ? and :

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,24 +15,14 @@ def text_indentation(text):
         TypeError: If text is not a string.
 
     Note:
-        This function indents the text by adding two new lines after each occurrence of the characters ".", "?", and ":".
-        Leading and trailing spaces are stripped from each character before printing.
-
-    Example:
-        >>> text_indentation("Hello! How are you? I am fine.")
-        Hello!
-        How are you?
-        I am fine.
-
+        There should be no space at the beginning or
+        at the end of each printed line.
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
     punctuation_marks = [".", "?", ":"]
-    for i, char in enumerate(text):
-        char = char.strip()
+    for char in text:
         print(char, end="")
-        if char in punctuation_marks and i < len(text) - 1:
-            next_char = text[i + 1]
-            if not next_char.isspace():
-                print("\n\n", end="")
+        if char in punctuation_marks:
+            print("\n\n", end="")
