a54347f2f7d946f9f0fa69e844d023d4e0065126
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-21 19:41:09 -0400

    empty class BaseGeometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that contains an empty class called BaseGeometry"""
+
+
+class BaseGeometry:
+    """
+    Empty class representing base geometry.
+
+    This class serves as a base class that can be inherited by other classes
+    to provide a common structure or functionality.
+    """
+    pass
