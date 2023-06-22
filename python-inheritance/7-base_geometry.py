44f67cbd83b5643bc4e9a537e7db22888896bcfd
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-21 20:33:46 -0400

    class BaseGeometry based on 6-base_geometry.py

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -3,40 +3,14 @@
 
 
 class BaseGeometry:
-    """
-    A class representing base geometry.
-
-    This class serves as a base class that can be inherited by other classes
-    to provide common geometry functionality.
-    """
+    """A class representing base geometry"""
 
     def area(self):
-        """
-        Calculate the area of the geometry.
-
-        Raises:
-            Exception: Indicates that the `area()` method is not implemented.
-
-        Returns:
-            None
-        """
+        """Calculate the area of the geometry"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Validate the given value as an integer.
-
-        Args:
-            name (str): The name of the value.
-            value: The value to be validated.
-
-        Raises:
-            TypeError: If the value is not an integer.
-            ValueError: If the value is less than or equal to 0.
-
-        Returns:
-            None
-        """
+        """Validate the given value as an integer"""
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
