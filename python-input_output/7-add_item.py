43390dc7dfcf549bca31b42a8e453375bc720bc3
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-22 20:19:06 -0400

    adds all arguments to a Python list, and then save them to a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,37 +1,21 @@
 #!/usr/bin/python3
 """
-This module contains a script that
-adds all arguments to a Python list
-and saves them to a file.
+This module contains a function that
+add all arguments to a Python list
+and save them to a file.
 """
-
-
 import sys
-import json
-from typing import List
 
 
-def save_to_json_file(my_obj: List[str], filename: str) -> None:
-    """Save a list object to a JSON file."""
-    with open(filename, 'w') as file:
-        json.dump(my_obj, file)
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
-def load_from_json_file(filename: str) -> List[str]:
-    """Load a list object from a JSON file."""
-    with open(filename, 'r') as file:
-        return json.load(file)
+try:
+    js_list = load_from_json_file("add_item.json")
+except Exception:
+    js_list = []
 
-
-def add_items_to_json_list():
-    """Add command line arguments to a Python list and
-    save them to a JSON file."""
-    filename = "add_item.json"
-    js_list = load_from_json_file(filename) if filename in sys.argv[1:] else []
-
-    js_list.extend(sys.argv[1:])
-    save_to_json_file(js_list, filename)
-
-
-if __name__ == "__main__":
-    add_items_to_json_list()
+for arg in sys.argv[1:]:
+    js_list.append(arg)
+save_to_json_file(js_list, "add_item.json")
