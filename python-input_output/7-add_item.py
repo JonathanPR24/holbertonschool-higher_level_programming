e7aaf6a4b0a6b1f38df2abb4e256c8a83b051c18
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-22 20:08:02 -0400

    adds all arguments to a Python list, and then save them to a file

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,41 @@
 #!/usr/bin/python3
 
 """
-This script adds all command line arguments to a
-Python list and saves them to a JSON file.
+This script adds all command line arguments to a Python
+list and saves them to a JSON file.
 """
 
 
 import sys
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+import json
 
+
+def save_to_json_file(my_obj, filename):
+    """
+    Save an object to a text file using JSON representation.
+
+    Args:
+        my_obj: The object to be saved.
+        filename (str): The name of the file to save the object to.
+
+    Returns:
+        None
+    """
+    with open(filename, 'w') as file:
+        json.dump(my_obj, file)
+
+def load_from_json_file(filename):
+    """
+    Load an object from a JSON file.
+
+    Args:
+        filename (str): The name of the JSON file.
+
+    Returns:
+        object: The Python object loaded from the JSON file.
+    """
+    with open(filename, 'r') as file:
+        return json.load(file)
 
 def add_items_to_json_list():
     """
