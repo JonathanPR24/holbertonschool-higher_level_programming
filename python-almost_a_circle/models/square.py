c45c06a70f3a013f9573a64abe01595f59a64046
JonathanPR24 <jonathanperez604@gmail.com>
2023-06-28 19:28:57 -0400

    class Square that inherits from Rectangle

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module contains the Base class.
+"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class that inherits from Rectangle.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+            x (int, optional): The x-coordinate of the square's position. Defaults to 0.
+            y (int, optional): The y-coordinate of the square's position. Defaults to 0.
+            id (int, optional): The ID of the square. Defaults to None.
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        Returns the string representation of the Square.
+
+        Returns:
+            str: The string representation of the Square.
+        """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
