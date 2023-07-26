1a50e694d9e8c24acd78b5a1bed32dd9e71bc9d1
JonathanPR24 <jonathanperez604@gmail.com>
2023-07-26 19:43:28 -0400

    Always a name

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,7 @@
 -- Script that creates the table force_name on your MySQL server.
 
-CREATE TABLE IF NOT EXISTS force_name (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
-);
+CREATE TABLE
+    IF NOT EXISTS force_name (
+        id INT,
+        name VARCHAR(256) NOT NULL
+    );
