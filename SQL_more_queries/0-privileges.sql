d43de5b9bace4afb00c46886fbf217d1a8b081fc
JonathanPR24 <jonathanperez604@gmail.com>
2023-07-26 19:27:44 -0400

    My privileges!

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- Connect to MySQL server
+
+-- Query for user_0d_1 privileges
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Query for user_0d_2 privileges
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
