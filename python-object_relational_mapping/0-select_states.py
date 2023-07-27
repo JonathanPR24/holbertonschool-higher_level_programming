f4e2541f08f9744868693b869b026ba9d6262e8d
JonathanPR24 <jonathanperez604@gmail.com>
2023-07-27 18:31:17 -0400

    script that lists all states from the database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python3
+"""Lists all states from the database hbtn_0e_0_usa"""
+
 import MySQLdb
 import sys
 
 def list_states(username, password, database):
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+    """
+    Lists all states from the specified database.
 
-    # Create a cursor to interact with the database
-    cursor = db.cursor()
+    Arguments:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Database name.
 
-    # Execute the query to select all states ordered by id in ascending order
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    Returns:
+        None
+    """
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
 
-    # Fetch all the results and print them as shown in the example
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
+        # Create a cursor to interact with the database
+        cursor = db.cursor()
 
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+        # Execute the query to select all states ordered by id in ascending order
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+        # Fetch all the results and print them as shown in the example
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print(f"Error connecting to the database: {e}")
+    finally:
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
