ca392bc72a1b9215e0739e77d8bbabd06487b3ad
JonathanPR24 <jonathanperez604@gmail.com>
2023-07-27 16:47:08 -0400

    script that lists all states from the database

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,24 +4,21 @@ import sys
 import MySQLdb
 
 def list_states(username, password, database_name):
-    db = None
-    cursor = None
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        db=database_name,
+        port=3306
+    )
+
+    # Create a cursor to interact with the database
+    cursor = db.cursor()
 
     try:
-        # Connect to the MySQL server
-        db = MySQLdb.connect(
-            host="localhost",
-            user=username,
-            passwd=password,
-            db=database_name,
-            port=3306
-        )
-
-        # Create a cursor to interact with the database
-        cursor = db.cursor()
-
         # Execute the SQL query to retrieve all states from the table
-        # in ascending order by id
+        # and sort them in ascending order by id
         cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
         # Fetch all rows from the result set
@@ -37,18 +34,20 @@ def list_states(username, password, database_name):
 
     finally:
         # Close the cursor and database connection
-        if cursor:
-            cursor.close()
-        if db:
-            db.close()
+        cursor.close()
+        db.close()
 
 if __name__ == "__main__":
+    # Check if all three arguments are provided (username, password,
+    # and database name)
     if len(sys.argv) != 4:
         print("Usage: python script_name.py <username> <password> <db_name>")
         sys.exit(1)
 
+    # Get the arguments from the command line
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
 
+    # Call the main function to list states from the database
     list_states(username, password, database_name)
