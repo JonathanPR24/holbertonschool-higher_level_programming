07add0d658b5282fa4501ffb5d2ca0a96b84a109
JonathanPR24 <jonathanperez604@gmail.com>
2023-08-01 14:44:57 -0400

    script that lists all states with a name starting with N

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,37 +1,58 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""
+Script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
-def list_states_with_N(username, password, database):
-    # Connect to the MySQL server running on localhost at port 3306
+
+def list_states_with_n(username, password, database_name):
     try:
-        conn = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-        cursor = conn.cursor()
+        # Connect to the MySQL server
+        db = MySQLdb.connect(
+            host="localhost",
+            user=username,
+            passwd=password,
+            db=database_name,
+            port=3306
+        )
 
-        # Execute the query to fetch states with names starting with 'N' in ascending order by states.id
-        query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-        cursor.execute(query)
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
 
-        # Fetch and display the results
-        results = cursor.fetchall()
-        for row in results:
+        # Execute the SQL query to retrieve all states with names starting with 'N'
+        cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+
+        # Fetch all rows from the result set
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
             print(row)
 
     except MySQLdb.Error as e:
-        print(f"Error connecting to MySQL server: {e}")
-        sys.exit(1)
+        # If there is an error, print the error message
+        print("Error:", e)
+
     finally:
-        # Close the database connection
+        # Close the cursor and database connection
         cursor.close()
-        conn.close()
+        db.close()
 
+
+# Check if the script is run as the main module
 if __name__ == "__main__":
+    # Check if all three arguments are provided (username, password, database name)
     if len(sys.argv) != 4:
-        print("Usage: ./1-filter_states.py <mysql_username> <mysql_password> <database_name>")
+        # Print the usage message and exit with an error code of 1
+        print("Usage: python script_name.py <username> <password> <db_name>")
         sys.exit(1)
 
+    # Get the arguments from the command line
     username = sys.argv[1]
     password = sys.argv[2]
-    database = sys.argv[3]
+    database_name = sys.argv[3]
 
-    list_states_with_N(username, password, database)
+    # Call the function to list states with names starting with 'N' from the database
+    list_states_with_n(username, password, database_name)
