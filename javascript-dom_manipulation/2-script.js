702149330d01dc8e4aaa6967c24d095f71255485
JonathanPR24 <jonathanperez604@gmail.com>
2023-09-18 09:19:31 -0400

    Add  class

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,14 +1,6 @@
-// 2-script.js
-document.addEventListener('DOMContentLoaded', function () {
-    // Select the element with id "red_header"
-    var redHeaderElement = document.getElementById('red_header');
-  
-    // Add a click event listener to it
-    redHeaderElement.addEventListener('click', function () {
-      // Select the header element
-      var headerElement = document.querySelector('header');
-  
-      // Add the class "red" to the header element
-      headerElement.classList.add('red');
-    });
-  });
+var red_header = document.getElementById('red_header')
+
+red_header.addEventListener('click', function(){
+    var header = document.querySelector('header');
+    header.classList.add("red");
+})
