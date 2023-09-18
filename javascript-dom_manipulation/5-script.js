aef623c4061f86b9759026f96c594d7c3aef5de0
JonathanPR24 <jonathanperez604@gmail.com>
2023-09-18 09:22:38 -0400

    Change the text

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,14 +1,7 @@
-// 5-script.js
-document.addEventListener('DOMContentLoaded', function () {
-    // Select the element with id "update_header"
-    var updateHeaderElement = document.getElementById('update_header');
-  
-    // Select the header element
-    var headerElement = document.querySelector('header');
-  
-    // Add a click event listener to the "update_header" element
-    updateHeaderElement.addEventListener('click', function () {
-      // Update the text content of the header element
-      headerElement.textContent = 'New Header!!!';
-    });
-  });
+var update_header = document.getElementById('update_header')
+
+update_header.addEventListener('click', function(){
+    var header = document.querySelector('header');
+
+    header.textContent = "New Header!!!"
+})
