1b83885ebae4ea4213fe6c3a12bfe3bd8143fe6a
JonathanPR24 <jonathanperez604@gmail.com>
2023-09-18 09:18:15 -0400

    Color Me

--- javascript-dom_manipulation/1-script.js
+++ javascript-dom_manipulation/1-script.js
@@ -1,14 +1,6 @@
-// 1-script.js
-document.addEventListener('DOMContentLoaded', function () {
-    // Select the element with id "red_header"
-    var redHeaderElement = document.getElementById('red_header');
-  
-    // Add a click event listener to it
-    redHeaderElement.addEventListener('click', function () {
-      // Select the header element
-      var headerElement = document.querySelector('header');
-  
-      // Update the text color to red
-      headerElement.style.color = '#FF0000';
-    });
-  });
+var red_header = document.getElementById('red_header')
+
+red_header.addEventListener('click', function(){
+    var header = document.querySelector('header');
+    header.style.color = '#FF0000'
+})
