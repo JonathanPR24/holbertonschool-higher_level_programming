c27e58e52f63ea38433799789b0886ef2c18338d
JonathanPR24 <jonathanperez604@gmail.com>
2023-09-18 09:26:30 -0400

    Star Wars movies

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,36 +1,22 @@
-// 7-script.js
-document.addEventListener('DOMContentLoaded', function () {
-    // Select the ul element with id "list_movies"
-    var listMoviesElement = document.getElementById('list_movies');
-  
-    // Define the URL of the API
-    var apiUrl = 'https://swapi-api.hbtn.io/api/films/?format=json';
-  
-    // Fetch data from the API
-    fetch(apiUrl)
-      .then(function (response) {
-        // Check if the response is successful (status code 200)
-        if (response.status === 200) {
-          // Parse the JSON response
-          return response.json();
-        } else {
-          // Handle the error
-          throw new Error('Failed to fetch data');
-        }
-      })
-      .then(function (data) {
-        // Extract the list of movies from the data
-        var movies = data.results;
-  
-        // Loop through the movies and add their titles to the list
-        movies.forEach(function (movie) {
-          var listItem = document.createElement('li');
-          listItem.textContent = movie.title;
-          listMoviesElement.appendChild(listItem);
-        });
-      })
-      .catch(function (error) {
-        // Handle any errors that occurred during the fetch
-        console.error(error);
-      });
-  });
+var list_movies = document.getElementById('list_movies')
+
+fetch("https://swapi-api.hbtn.io/api/films/?format=json")
+.then(function(response){
+    if (!response.ok){
+        throw new Error("Error on Network response")
+    }
+    return response.json();
+})
+.then(function(data){
+    console.log(data)
+    data.results.forEach(function(movie){
+        var movie_title = document.createElement('li');
+
+        movie_title.textContent = movie.title;
+
+        list_movies.appendChild(movie_title);
+    });
+})
+.catch(function(error){
+    console.error("There was a problem with the fetch operation:", error);
+});
