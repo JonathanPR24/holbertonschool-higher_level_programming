224249b11cd4fab080d48739e6e41ba451b5257d
JonathanPR24 <jonathanperez604@gmail.com>
2023-09-18 09:22:30 -0400

    List of elements

--- javascript-dom_manipulation/4-script.js
+++ javascript-dom_manipulation/4-script.js
@@ -1,20 +1,11 @@
-// 4-script.js
-document.addEventListener('DOMContentLoaded', function () {
-    // Select the element with id "add_item"
-    var addItemElement = document.getElementById('add_item');
-  
-    // Select the <ul> element with class "my_list"
-    var myList = document.querySelector('.my_list');
-  
-    // Add a click event listener to the "add_item" element
-    addItemElement.addEventListener('click', function () {
-      // Create a new <li> element
-      var newItem = document.createElement('li');
-  
-      // Set the text content of the new <li> element to "Item"
-      newItem.textContent = 'Item';
-  
-      // Append the new <li> element to the <ul> element
-      myList.appendChild(newItem);
-    });
-  });
+var add_item = document.getElementById('add_item')
+
+add_item.addEventListener('click', function(){
+    var element = document.createElement('li');
+    
+    element.textContent = "Item";
+
+    var my_list = document.querySelector('.my_list');
+
+    my_list.appendChild(element)
+})
